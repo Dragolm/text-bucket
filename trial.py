5065b36d53b943ff6b7282653182be232da62255
Dragolm <133989197+Dragolm@users.noreply.github.com>
2024-12-05 20:07:37 +0530

    Add files via upload

--- trial.py
+++ trial.py
@@ -1,11 +1,25 @@
-fr = open('mdinput.md','r');
-lines=[];
-for line in fr:
-	print(line);
-	newline=line.replace('I',"ME");
-	lines.append(newline);
-	print(newline);
-print("done reading");
-
-fw = open('mdinput.md','w');
-fw.writelines(lines);
+words=[];
+para=[];
+replaceables={"I":"you", "my":"your"};
+flag=False;
+open("output.txt","w").close();
+with open('input.txt','r') as inp, open('output.txt', 'a') as out:
+	for line in inp:
+		para.append(line.split());
+	for words in para:
+		for word in words:
+			if(word[0]=='"' or flag== True):
+				out.write(word+" ");
+#				out.write("\n");
+				flag=True;
+			else:
+				if word in replaceables:
+					out.write(replaceables.get(word)+" ");
+#					out.write("\n");
+				else:
+					out.write(word+" ");
+#					out.write("\n");
+			if(word[-1]=='"'):
+				flag = False;
+		out.write("\n");
+#print(words);
